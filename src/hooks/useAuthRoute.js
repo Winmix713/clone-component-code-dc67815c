00b3747d83a3557692ba6889f8a530f5db3c0aec
Winmix713 <takosadam@gmail.com>
2025-12-05 11:51:40 +0100

    Update useAuthRoute.js

--- src/hooks/useAuthRoute.js
+++ src/hooks/useAuthRoute.js
@@ -1,21 +1,11 @@
-import * as React from 'react';
 import {useLocation} from 'react-router-dom';
 
 const useAuthRoute = () => {
     const authRoutes = ['/login', '/sign-up'];
     const location = useLocation();
-    const [isAuthRoute, setIsAuthRoute] = React.useState(false);
-
-    React.useEffect(() => {
-        setIsAuthRoute(authRoutes.includes(location.pathname));
-
-        return () => {
-            setIsAuthRoute(false);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [location]);
-
-    return isAuthRoute;
+    
+    // Simply return the boolean value derived from the current location
+    return authRoutes.includes(location.pathname);
 }
 
 export default useAuthRoute
